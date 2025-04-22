17d32c6ad9e260c53e757d2abd93cf793928327c
Jacob Christensen <jacobchristensen811@gmail.com>
2025-04-22 14:10:17 -0600

    changed the app name to a better temp name

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-2xl font-semibold">My App</div>
+        <div className="text-white text-2xl font-semibold">MyFantasyCharacter WIP</div>
         <div>
           <ul className="flex space-x-4">
             <li>
